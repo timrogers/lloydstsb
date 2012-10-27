360fb52e1f7bc4973594cb4df50dc20df97796d6
Tim Rogers <tim@gocardless.com>
2012-10-27 15:17:31 +0100

    Adds lots of comments to the main file

--- lib/customer.rb
+++ lib/customer.rb
@@ -7,29 +7,41 @@ require 'date'
 
 module LloydsTSB
   class Customer
-    
+
     attr_reader :agent
-    
+
     def initialize(settings = {})
+      # Creates a new Customer object - expects a hash with keys :username,
+      # :password and :memorable_word
       @agent = Mechanize.new
       @settings = settings
-      
-      if @settings[:username].blank? || @settings[:password].blank? || @settings[:memorable_word].blank?
-        raise "You must provide a username, password and memorable word."
+
+      if @settings[:username].blank? ||
+        @settings[:password].blank? ||
+        @settings[:memorable_word].blank?
+          raise "You must provide a username, password and memorable word."
       end
-      
+
       @agent.get "https://online.lloydstsb.co.uk/personal/logon/login.jsp?WT.ac=hpIBlogon"
     end
-    
+
     def accounts
+      # Fills in the relevant forms to login, gets account details and then
+      # provides a response of accounts and transactions
+      
+      # Fill in the first authentication form then submits
       @agent.page.forms[0]["frmLogin:strCustomerLogin_userID"] = @settings[:username]
       @agent.page.forms[0]["frmLogin:strCustomerLogin_pwd"] = @settings[:password]
       @agent.page.forms[0].submit
       
+      # Checks for any errors on the page indicating a failure to login
       if @agent.page.search('.formSubmitError').any?
-        raise "There was a problem when submitting your username and password. (#{@agent.page.search('.formSubmitError').text})"
+        raise "There was a problem when submitting your username and password.
+          (#{@agent.page.search('.formSubmitError').text})"
       end
       
+      # Works out from the text on the page what characters from the memorable
+      # word are required
       mc1 = @agent.page
         .at('//*[@id="frmentermemorableinformation1"]/fieldset/div/div/div[1]/label').text.split(" ")[1].to_i
       mc2 = @agent.page
@@ -37,60 +49,85 @@ module LloydsTSB
       mc3 = @agent.page.
         at('//*[@id="frmentermemorableinformation1"]/fieldset/div/div/div[3]/label')
           .text.split(" ")[1].to_i
-          
+        
+      # Files in the memorable word fields and logs in
       @agent.page.forms[0]["frmentermemorableinformation1:strEnterMemorableInformation_memInfo1"] = "&nbsp;" + @settings[:memorable_word][mc1-1]
       @agent.page.forms[0]["frmentermemorableinformation1:strEnterMemorableInformation_memInfo2"] = "&nbsp;" + @settings[:memorable_word][mc2-1]
       @agent.page.forms[0]["frmentermemorableinformation1:strEnterMemorableInformation_memInfo3"] = "&nbsp;" + @settings[:memorable_word][mc3-1]
       @agent.page.forms[0].click_button
-      
+
+      # Checks for any errors indicating a failure to login - the final hurdle
       if @agent.page.search('.formSubmitError').any?
-        raise "There was a problem when submitting your memorable word. (#{@agent.page.search('.formSubmitError').text})"
+        raise "There was a problem when submitting your memorable word.
+          (#{@agent.page.search('.formSubmitError').text})"
       end
-      
+
+      # We're in, now to find the accounts...
       accounts = []
       doc = Nokogiri::HTML(@agent.page.body, 'UTF-8')
        doc.css('li.clearfix').each do |account|
+        # This is an account in the table - let's read out the details...
         acct = {
           name: account.css('a')[0].text,
           identifier: account.css('.numbers').text,
-          balance: account.css('p.balance').text.split(" ")[1].gsub("£", "").gsub(",", "").to_f,
-          limit: account.css('p.accountMsg').text.split(" ")[2].gsub("£", "").gsub(",", "").to_f,
+          balance: account.css('p.balance').text.split(" ")[1]
+            .gsub("£", "").gsub(",", "").to_f,
+          limit: account.css('p.accountMsg').text.split(" ")[2]
+            .gsub("£", "").gsub(",", "").to_f,
           transactions: []
           }
-         
-         
+
+        # Now we need to find the recent transactions for the account...We'll
+        # go to the account's transactions page and read the table
         account_agent = @agent.dup
         account_agent.get(account.css('a')[0]['href'])
+        
+        # If there's a mention of "minimum payment" on the transactions page,
+        # this is a credit card rather than a bank account
         if account_agent.page.body.include?("Minimum payment")
-          # This is a credit card account
           acct[:type] = :credit_card
           Nokogiri::HTML(account_agent.page.body, 'UTF-8').css('tbody tr').each do |transaction|
+            
+            # Credit card statements start with the previous statement's
+            # balance. We don't want to record this as a transaction.
             next if transaction.css('td')[1].text == "Balance from Previous Statement"
+            
+            # Let's get the data for the transaction...
             data = {
               date: Date.parse(transaction.css('td')[0].text),
               narrative: transaction.css('td')[1].text,
             }
             data[:amount] = transaction.css('td')[4].text.split(" ")[0].to_f
+            
+            # And now we work out whether the transaction was a credit or
+            # debit by checking, in a round-about way, whether the
+            # transaction amount contained "CR" (credit)
             if transaction.css('td')[4].text.split(" ").length > 1
               data[:type] = :credit
+              data[:direction] = :credit
             else
               data[:type] = :debit
+              data[:direction] = :debit
             end
+            
+            # And finally, we add the transaction object to the array
             acct[:transactions] << LloydsTSB::Transaction.new(data)
           end
         else
           # This is a bank account of some description
           acct[:type] = :bank_account
           Nokogiri::HTML(account_agent.page.body, 'UTF-8').css('tbody tr').each do |transaction|
-            next if transaction.css('td')[1].text == "Balance from Previous Statement"
+            # Let's read the details from the table...
             data = {
               date: Date.parse(transaction.css('th.first').text),
               narrative: transaction.css('td')[0].text,
               type: transaction.css('td')[1].text.to_sym,
             }
+            
+            # Regardless of what the transaction is, there's an incoming
+            # and an outgoing column. Let's work out which this is...
             incoming = transaction.css('td')[2].text
             out = transaction.css('td')[3].text
-          
             if incoming == ""
               data[:direction] = :debit
               data[:amount] = out.to_f
@@ -98,17 +135,15 @@ module LloydsTSB
               data[:direction] = :credit
               data[:amount] = incoming.to_f
             end
+            
+            # To finish, we add the newly built transaction to the array
             acct[:transactions] << LloydsTSB::Transaction.new(data)
           end
         end
-        
-          
-        #acct[:transactions] << LloydsTSB::Transaction.new(data)
-          
-        
+
         accounts << LloydsTSB::Account.new(acct)
       end
       accounts
-    end   
+    end
   end
 end
--- lloyds_tsb.rb
+++ lloyds_tsb.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # Bring in all the files in lib/, including the scraper and the data models
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 
@@ -9,5 +10,18 @@ require File.join(File.dirname(__FILE__), 'settings')
 customer = LloydsTSB::Customer.new(@settings)
 
 customer.accounts.each do |account|
-  
+  puts "Name: #{account.name}"
+  puts "Type: #{account.type.to_s}"
+  puts "Balance: £#{account.balance}"
+  puts "Limit: £#{account.limit}"
+  puts "Transactions:"
+  puts ""
+  account.transactions.each do |tx|
+    puts "Date: #{tx.date}"
+    puts "Description: #{tx.narrative}"
+    puts "Type: #{tx.type}"
+    puts "Direction: #{tx.direction}"
+    puts "Amount: £#{tx.amount}"
+    puts ""
+  end
 end
